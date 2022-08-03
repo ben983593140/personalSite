0145da325e608b4130d5794d65f966f07f6e2c7c
ben983593140 <ben983593140@gmail.com>
2022-08-02 22:26:39 -0700

    added logo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import NavBar from './NavBar.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <p>
-          And this is the newly added sentence.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar></NavBar>
     </div>
-  );
+);
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload
+//         </p>
+//         <p className="DarkChar">
+//           Nope!
+//         </p>
+//         <p>
+//           New!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import logo from './logo.png';
+import "./Color.css";
+import "./Style.css";
+
+
+function NavBar() {
+    return (
+        <div className="Logo">
+            <img src={logo} />
+        </div>
+    );
+}
+
+export default NavBar;
